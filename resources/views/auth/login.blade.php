3531b68c26205d3f9aae3872e151749d3dcde7af
Zemog gomez <zemogmjg@gmail.com>
2024-05-06 16:01:09 -0500

    Configuracion de las vistas de inicio y actualizacion de datos.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,57 @@
 <x-guest-layout>
-    <!-- Session Status -->
+    <!-- Estado de la sesión -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
-
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+    <div class="flex">
+        <!-- Imagen en la parte izquierda -->
+        <div class="w-screen h-screen bg-left bg-cover bg-no-repeat" style="background-image: url('{{ asset('images/loginini.png') }}')">
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <!-- Formulario de inicio de sesión en la parte derecha -->
+        <div class="w-full md:w-5/12 lg:w-1/2 bg-white p-3">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+                <!-- Dirección de correo electrónico -->
+                <div class="mb-4">
+                    <x-input-label for="email" :value="__('Correo electrónico')" />
+                    <x-text-input id="email" class="block w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                <!-- Contraseña -->
+                <div class="mb-4">
+                    <x-input-label for="password" :value="__('Contraseña')" />
+
+                    <x-text-input id="password" class="block w-full"
+                                    type="password"
+                                    name="password"
+                                    required autocomplete="current-password" />
+
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+
+                <!-- Recordarme -->
+                <div class="mb-4">
+                    <label for="remember_me" class="inline-flex items-center">
+                        <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                        <span class="ms-2 text-sm text-gray-600">{{ __('Recordarme') }}</span>
+                    </label>
+                </div>
+
+                <div class="flex items-center justify-end">
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                            {{ __('¿Olvidaste tu contraseña?') }}
+                        </a>
+                    @endif
+
+                    <x-primary-button class="ms-3">
+                        {{ __('Iniciar sesión') }}
+                    </x-primary-button>
+              
+                </div>
+            </form>
         </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
+    </div>
 </x-guest-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,12 +32,12 @@
                                 </a>
                             @else
                                 <a href="{{ route('login') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                                    Inicio sesión
+                                    Iniciar sesión
                                 </a>
 
                                 @if (Route::has('register'))
                                     <a href="{{ route('register') }}" class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white">
-                                        Registro
+                                        Registrarse
                                     </a>
                                 @endif
                             @endauth
@@ -45,7 +45,7 @@
                     @endif
                 </header>
 
-                <body class="font-sans antialiased dark:bg-black dark:text-white/50" style="background-image: url('{{ asset('images/home.png') }}'); background-size: cover; background-position: center;">
+                <body class="font-sans antialiased dark:bg-black dark:text-white/50" style="background-image: url('{{ asset('images/newfondom.png') }}'); background-size: cover; background-position: center;">
     <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
         <div class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
 </div>
