ecca8f5412ef170cbe287dff7950c4f704a28577
Zemog gomez <zemogmjg@gmail.com>
2024-05-16 20:19:46 -0500

    Configuracion usuario

--- resources/views/usuario/index.blade.php
+++ resources/views/usuario/index.blade.php
@@ -1,82 +1,119 @@
 <!doctype html>
 <html lang="en">
-
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <title>Home</title>
+
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+
+        .navbar {
+            background-color: #007bff;
+            color: #fff;
+        }
+
+        .navbar-brand {
+            color: #fff;
+            font-weight: bold;
+        }
+
+        .card {
+            border: none;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+            transition: box-shadow 0.3s ease;
+        }
+
+        .card:hover {
+            box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+        }
+
+        .card-img-top {
+            border-top-left-radius: 10px;
+            border-top-right-radius: 10px;
+        }
+
+        .card-body {
+            padding: 1.5rem;
+        }
+
+        .card-title {
+            font-weight: bold;
+        }
+
+        .btn-primary {
+            background-color: #007bff;
+            border-color: #007bff;
+        }
+
+        .btn-primary:hover {
+            background-color: #0069d9;
+            border-color: #0062cc;
+        }
+    </style>
 </head>
-
 <body>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('images/vision-48.png') }}" alt="" width="30" height="24" class="d-inline-block align-text-top">
+                Vision Stock
+            </a>
+            <a class="navbar-brand" href="#">
+                <img src="{{ asset('images/home-48.png') }}" alt="" width="30" height="24" class="d-inline-block align-text-top">
+                Home
+            </a>
+        </div>
+    </nav>
 
-<ul class="nav nav-tabs">
-  <li class="nav-item">
-  <a class="navbar-brand" href="#" >
-      <img src="{{ asset('images/vision-48.png') }}" alt="" width="30" height="24" class="d-inline-block text-align: left">
-      Vision Stock
-    </a>
-  </li>
-  <li class="nav-item">
-  <a class="navbar-brand" href="#" >
-    <img src="{{ asset('images/home-48.png') }}" alt="" width="30" height="24" class="d-inline-block text-align: left">
-    Home
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-  </li>
-</ul>
-<div class="container my-5">
-
-<style>
-  .card-container {
-    display: flex;
-    justify-content: space-between; /* Distribuye los elementos horizontalmente */
-    margin-bottom: 20px; /* Margen entre las filas */
-  }
-
-  .card {
-    flex: 0 0 calc(33.333% - 10px); /* Ajusta el tamaño de las tarjetas para que haya espacio entre ellas */
-  }
-</style>
-
-<div class="card-container">
-  <div class="card" style="width: 18rem;">
-    <img src="{{ asset('images/user/lentes.jpg') }}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Lentes de contacto</h5>
-      <p class="card-text">Hola mundo</p>
-      <a href="{{ route('usuario_producto1') }}" class="btn btn-primary">Detalle</a>
+    <div class="container my-5">
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                    <img src="{{ asset('images/user/lentes.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Lentes de contacto</h5>
+                        <p class="card-text">Hola mundo</p>
+                        <a href="{{ route('usuario_producto1') }}" class="btn btn-primary">Detalle</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="{{ asset('images/user/gota_hidratante.jpg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Gotas hidratantes</h5>
+                        <p class="card-text">Hola mundo</p>
+                        <a href="{{ route('usuario_producto2') }}" class="btn btn-primary">Detalle</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="{{ asset('images/user/gafa.jpeg') }}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Gafas</h5>
+                        <p class="card-text">Hola mundo</p>
+                        <a href="{{ route('usuario_producto3') }}" class="btn btn-primary">Detalle</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
 
-  <div class="card" style="width: 18rem;">
-    <img src="{{ asset('images/user/gota_hidratante.jpg') }}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Gotas hidratantes</h5>
-      <p class="card-text">Hola mundo</p>
-      <a href="{{ route('usuario_producto2') }}" class="btn btn-primary">Detalle</a>
-    </div>
-  </div>
-
-  <div class="card" style="width: 18rem;">
-    <img src="{{ asset('images/user/gafa.jpeg') }}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">Gafas</h5>
-      <p class="card-text">Hola mundo</p>
-      <a href="{{ route('usuario_producto3') }}" class="btn btn-primary">Detalle</a>
-    </div>
-  </div>
-
-</div>
-
-
-
-</div>
+    <!-- Optional JavaScript; choose one of the two! -->
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
+</html>
--- resources/views/usuario_producto/producto1.blade.php
+++ resources/views/usuario_producto/producto1.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vista de Producto</title>
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
         .container {
             display: flex;
@@ -12,7 +12,6 @@
             align-items: center;
             height: 100vh;
         }
-
         .card {
             width: 18rem;
             margin-left: 100px; /* Ajusta este valor según sea necesario */
@@ -22,13 +21,19 @@
 </head>
 <body>
     <div class="container">
-        <div class="card" style="width: 18rem;">
+        <div class="card">
             <img src="{{ asset('images/user/lentes.jpg') }}" class="card-img-top" alt="Imagen del producto">
             <div class="card-body">
                 <h5 class="card-title">Lentes de contacto</h5>
-                <p class="card-text">Descripción del Producto</p>
+                <p class="card-text">Descripción del Producto: </p>
+                <p class="card-text">Costo: </p>
+                <p class="card-text">Stock disponible: </p>
+                <p class="card-text">Recomendaciones de uso: </p>
+                <a href="{{ route('pagos.index') }}" class="btn btn-primary">Comprar</a>
             </div>
         </div>
     </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 </html>
